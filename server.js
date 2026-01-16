7ca2218755279e7a26f3f83ea42b38ee5191c61c
Muskan2518 <muskan220405@gmail.com>
2026-01-16 19:29:02 +0530

    first commit:

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./database/connect");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Node.js + Express!");
+// Connect MongoDB
+connectDB();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "UP helathy",
+    db: "CONNECTED",
+    timestamp: new Date().toISOString(),
+  });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
